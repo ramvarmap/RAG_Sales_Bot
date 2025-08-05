7e11357f8a0282521eb3d31d80da4b5f707631b8
ramvarmap <ram.penumatsa@itversity.com>
2025-08-05 12:56:49 +0530

    Fix Terraform outputs - reference data sources instead of resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,15 +7,15 @@ output "cloud_run_url" {
 
 output "cloud_run_service_account" {
   description = "Email of the service account used by Cloud Run"
-  value       = google_service_account.cloud_run_sa.email
+  value       = data.google_service_account.cloud_run_sa.email
 }
 
 output "artifact_registry_location" {
   description = "Location of the Artifact Registry repository"
-  value       = google_artifact_registry_repository.smart_chart.location
+  value       = data.google_artifact_registry_repository.smart_chart.location
 }
 
 output "artifact_registry_name" {
   description = "Name of the Artifact Registry repository"
-  value       = google_artifact_registry_repository.smart_chart.name
+  value       = data.google_artifact_registry_repository.smart_chart.name
 } 
